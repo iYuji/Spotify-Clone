5fe165b919916310a84f0c65112879856e5a2738
unknown <raphaelyujinakano@icloud.com>
2023-11-02 18:49:26 -0300

    defininido tokens

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit{
 
-  constructor() {}
+  constructor(private spotifyService: SpotifyService) {}
 
   ngOnInit(): void {
-      
+    this.verificarTokenUrlCallback();
+  }
+  
+  verificarTokenUrlCallback() {
+    const token = this.spotifyService.obterTokenUrlCallback();
+    if(!!token){
+      this.spotifyService.definirAccessToken(token);
+    }
+  }
+
+  abrirPaginaLogin(){
+    window.location.href = this.spotifyService.obterUrlLogin();
   }
 
 }
